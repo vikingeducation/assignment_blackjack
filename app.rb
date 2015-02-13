a52e6fea61c16a380dc65d43ad2a3867480aecca
Dan Berger <dsberger@gmail.com>
2015-02-12 23:51:58 -0800

    working deal and hand viewing. Hit and stay buttons added but not functional

--- app.rb
+++ app.rb
@@ -2,10 +2,18 @@ require 'sinatra'
 require 'pry-byebug'
 require 'erb'
 
+enable :sessions
+
 get '/' do
   redirect to('blackjack')
 end
 
 get '/blackjack' do
-  erb :"blackjack"
+  session[:deck] = [2,3,4,5,6,7,8,9,10,:J,:Q,:K,:A] * 4
+  session[:deck].shuffle!
+  session[:hole_card] = session[:deck].pop
+  session[:player_hand] = [session[:deck].pop]
+  session[:dealer_upcards] = [session[:deck].pop]
+  session[:player_hand] << session[:deck].pop
+  erb :blackjack
 end
