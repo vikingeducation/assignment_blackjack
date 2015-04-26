7458f3dd851124679f2e21c627c5caef39fff229
niconan <nick.hax@gmail.com>
2015-04-25 20:51:55 -0700

    working, now for cleanup

--- app.rb
+++ app.rb
@@ -9,17 +9,21 @@ get '/' do
 end
 
 get '/blackjack' do
-  session[:deck] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :K, :Q] * 4
+  session[:deck] = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :K, :Q, :A] * 4
   session[:deck].shuffle!
   session[:player_hand] = [session[:deck].pop]
   session[:player_hand] << session[:deck].pop
   session[:dealer_hand] = [session[:deck].pop]
   session[:dealer_hand] << session[:deck].pop
+  redirect to '/blackjack/stay' if player_blackjack?
+  redirect to 'blackjack/stay' if dealer_blackjack?
   erb :blackjack
 end
 
 get '/blackjack/hit' do
   session[:player_hand] << session[:deck].pop
+  redirect to '/blackjack/stay' if player_bust?
+  redirect to 'blackjack/stay' if player_blackjack?
   erb :blackjack
 end
 
@@ -28,4 +32,5 @@ get '/blackjack/stay' do
     session[:dealer_hand] << session[:deck].pop
   end
   erb :blackjack
+  erb :result
 end
--- helpers/handhelper.rb
+++ helpers/handhelper.rb
@@ -2,20 +2,35 @@ module HandHelper
 
   def value_of_hand(cards)
     sum = 0
-    cards.each { |card| sum += value_of_card(card) }
+    cards.each { |card| sum += value_of_card(card, sum) }
     return sum
   end
 
-  def value_of_card(card)
+  def value_of_card(card, sum)
     if [:J, :K, :Q].include?(card)
       return 10
+    elsif card == :A
+      return 1 if sum + 11 > 21
+      return 11 if sum + 11 <= 21
     else
       return card
     end
   end
 
-  def lost?
+  def dealer_blackjack?
+    value_of_hand(session[:dealer_hand]) == 21
+  end
+
+  def player_blackjack?
+    value_of_hand(session[:player_hand]) == 21
+  end
+
+  def player_bust?
     value_of_hand(session[:player_hand]) > 21
+  end
+
+  def dealer_bust?
+    value_of_hand(session[:dealer_hand]) > 21
   end
 
 end
