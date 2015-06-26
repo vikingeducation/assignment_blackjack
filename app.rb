11a14ef0b300fc1687e5f5d17d4f6367b53e7231
Adam Kinson <adam.kinson@gmail.com>
2015-06-26 16:31:59 -0400

    build deck, deal and render hands

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
 
+
 get '/' do
   erb :home
+end
+
+
+get '/blackjack' do
+  # shuffle cards
+  deck = ( (2..10).to_a.map {|n| n.to_s} + ["J","Q","K","A"] )*4
+  deck.shuffle!
+
+  # deal hands
+  @player_hand, @dealer_hand = [], [
+  ]
+  2.times do
+    @player_hand << deck.pop
+    @dealer_hand << deck.pop
+  end
+
+  # render
+  erb :blackjack
 end
