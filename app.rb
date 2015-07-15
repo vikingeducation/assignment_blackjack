116c51b614a3af26d65234f2e5c4748d42802fb3
mikebutts <mgbutts@gmail.com>
2015-07-15 13:09:21 -0400

    blackjack

--- app.rb
+++ app.rb
@@ -6,35 +6,8 @@ require 'pry'
 
 enable :sessions
 
-include Blackjack
-
 helpers do
-
-  def initial_hands
-    hand1 = []
-    hand2 = []
-    deal([], hand1)
-    deal(hand1, hand2)
-    deal(hand1 + hand2, hand1)
-    deal(hand1 + hand2, hand2)
-
-    return hand1, hand2
-  end
-
-  def card_to_hand(card)
-    faces = {1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King"}
-    suits = {0 => "spades", 1 => "clubs", 2 => "hearts", 3 => "diamonds"}
-    ret = ""
-    if faces[(card % 13) + 1]
-      value = faces[(card % 13) + 1]
-    else
-      value = (card % 13) + 1
-    end
-    ret += value.to_s
-    ret += " of "
-    ret += suits[(card / 13)]
-  end
-
+ include Blackjack
 end
 
 
@@ -54,11 +27,12 @@ post '/blackjack/hit' do
   deal(d_hand + p_hand, p_hand)
   session['d_hand'] = d_hand
   session['p_hand'] = p_hand
-  redirect '/blackjack/stay', locals: {d_hand: d_hand, p_hand: p_hand} if bust(p_hand)
+  redirect '/blackjack/gameover', locals: {d_hand: d_hand, p_hand: p_hand} if bust(p_hand)
   erb :blackjack, locals: {d_hand: d_hand, p_hand: p_hand}
 end
 
-get '/blackjack/stay' do
+post '/blackjack/stay' do
   d_hand, p_hand = session['d_hand'], session['p_hand']
   erb :stay, locals: {d_hand: d_hand, p_hand: p_hand}
+  
 end
--- blackjack.rb
+++ blackjack.rb
@@ -7,9 +7,50 @@ module Blackjack
     target_hand.push(cards.sample)
     target_hand
   end
+  
+  def initial_hands
+    hand1 = []
+    hand2 = []
+    deal([], hand1)
+    deal(hand1, hand2)
+    deal(hand1 + hand2, hand1)
+    deal(hand1 + hand2, hand2)
+
+    return hand1, hand2
+  end
+  
+
+  #strictly for showing the hand to player
+  def card_to_hand(card)
+    faces = {1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King"}
+    suits = {0 => "spades", 1 => "clubs", 2 => "hearts", 3 => "diamonds"}
+    ret = ""
+    if faces[(card % 13) + 1]
+      value = faces[(card % 13) + 1]
+    else
+      value = (card % 13) + 1
+    end
+    ret += value.to_s
+    ret += " of "
+    ret += suits[(card / 13)]
+  end
 
   def bust (hand)
     hand.reduce(0){|acc, val| acc += (val % 13 + 1)} > 21
+  end
+  
+  def value_hand(hand)
+    aces, other = hand.partition{|val| val % 13 == 0}
+    sum = other.reduce(0){|acc, val| acc += (val % 13 + 1)}
+    until aces.empty?
+      current = aces.pop
+      if sum + 11 > 21
+        sum += 1
+      else
+        sum += 11
+      end
+    end
+    sum
   end
 
   def dealer_hand
