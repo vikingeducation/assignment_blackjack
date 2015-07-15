1a7a5274051e1edaa191890f9d5cea579b8b4617
facingsouth <xin.d.liu@gmail.com>
2015-07-15 15:41:52 -0700

    merge

--- app.rb
+++ app.rb
@@ -17,11 +17,14 @@ get "/" do
 end
 
 get "/bet" do 
-  erb :bet, :locals => {:bankroll => session[:bankroll]}
+  erb :bet, :locals => {:bankroll => session[:bankroll], 
+                        :message => session[:not_enough_money]}
 end
 
 post "/bet" do
-  erb :bet, :locals => {:bankroll => session[:bankroll]}
+  session[:not_enough_money] = nil
+  erb :bet, :locals => {:bankroll => session[:bankroll], 
+                        :message => session[:not_enough_money]}
 end
 
 get "/blackjack" do
@@ -36,51 +39,55 @@ get "/blackjack" do
 end
 
 post "/blackjack" do
-  session[:bet] = params[:bet].to_i
-  session[:bankroll]-=params[:bet].to_i
-  
-  if session[:gamestate] == true
-  session[:dealercards] = []
-  session[:playercards] = []
-  session[:bet] = 0
-  # session[:bankroll] = 1000
-  end
-
-  game=Game.new(session[:dealercards], session[:playercards])
-  choice = params[:choice]
-  if choice == "Deal"
-    game.deal
-    if game.game_over(choice)
-      session[:gamestate] = true
-    else
-      session[:gamestate] = false
-    end
-
-  elsif choice == "Hit"
-    game.hit
-  elsif choice == "Stand"
-    game.stand
-  end
-
-  #Check for winner
-  if game.game_over(choice)
-    case session[:message]
-    when "Winner!"
-      session[:bankroll] += 2*session[:bet]
-    when "Tie!"
-      session[:bankroll] += session[:bet]
-    end
-
-    session[:gamestate] = true
-
-    session[:message] = game.game_over(choice)
-
-    redirect '/blackjack/stay'
+  if params[:bet].to_i > session[:bankroll]
+    session[:not_enough_money] = "You don't have enough money"
+    redirect "/bet"
   else
-    save_game(game.dealercards, game.playercards, session[:gamestate])  
-  end
+    session[:bet] = params[:bet].to_i
+    session[:bankroll]-=params[:bet].to_i
+    
+    if session[:gamestate] == true
+    session[:dealercards] = []
+    session[:playercards] = []
+    # session[:bet] = 0
+    # session[:bankroll] = 1000
+    end
 
-  erb :game, :locals => {:dealercards => game.dealercards, :playercards => game.playercards, :gamestate => session[:gamestate], :bankroll => session[:bankroll]}
+    game=Game.new(session[:dealercards], session[:playercards])
+    choice = params[:choice]
+    if choice == "Deal"
+      game.deal
+      if game.game_over(choice)
+        session[:gamestate] = true
+      else
+        session[:gamestate] = false
+      end
+
+    elsif choice == "Hit"
+      game.hit
+    elsif choice == "Stand"
+      game.stand
+    end
+
+    #Check for winner
+    if game.game_over(choice)
+      session[:message] = game.game_over(choice)
+      case session[:message]
+      when "Winner!"
+        session[:bankroll] += 2*session[:bet]
+      when "Tie!"
+        session[:bankroll] += session[:bet]
+      end
+
+      session[:gamestate] = true
+
+      redirect '/blackjack/stay'
+    else
+      save_game(game.dealercards, game.playercards, session[:gamestate])  
+    end
+
+    erb :game, :locals => {:dealercards => game.dealercards, :playercards => game.playercards, :gamestate => session[:gamestate], :bankroll => session[:bankroll]}
+  end
 
 #playercards, dealercards, winner = Game(playercards,dealercards)
 
@@ -99,4 +106,8 @@ get '/blackjack/stay' do
 
   erb :stay, :locals => {:dealercards => session[:dealercards], :playercards => session[:playercards], :message => session[:message]} 
 
+end
+
+get "/not_enough_money" do
+  erb :not_enough_money
 end
