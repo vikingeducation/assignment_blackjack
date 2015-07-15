18ce8ec42832b74f340a4620fb863bcffb6e1631
shadefinale <shadefinale@gmail.com>
2015-07-15 13:38:49 -0500

    Refactored results screen into single .erb file.

--- app.rb
+++ app.rb
@@ -7,18 +7,30 @@ require 'pry'
 enable :sessions
 
 helpers do
- include Blackjack
+  include Blackjack
 
   def check_winner(d_hand,p_hand)
-    
+
     if blackjack?(d_hand) && blackjack?(p_hand)
-      redirect '/blackjack/tie', locals: {d_hand: session['d_hand'], p_hand: session['p_hand']}
+      tie
     elsif blackjack?(d_hand) && !blackjack?(p_hand)
-    redirect '/blackjack/gameover', locals: {d_hand: session['d_hand'], p_hand: session['p_hand']}
+      game_over
     elsif blackjack?(p_hand)
-      redirect '/blackjack/win', locals: {d_hand: session['d_hand'], p_hand: session['p_hand']}
+      win
     end
 
+  end
+
+  def game_over
+    redirect "/blackjack/result/You Lose..."
+  end
+
+  def win
+    redirect "/blackjack/result/You Win!"
+  end
+
+  def tie
+    redirect "/blackjack/result/You Pushed!"
   end
 end
 
@@ -33,29 +45,27 @@ get '/blackjack' do
   session['p_hand'] = p_hand
 
   check_winner(d_hand,p_hand)
-
-
   erb :blackjack, locals: {d_hand: d_hand, p_hand: p_hand}
 end
 
 post '/blackjack/hit' do
   d_hand, p_hand = session['d_hand'], session['p_hand']
   deal(d_hand + p_hand, p_hand)
-  session['d_hand'] = d_hand
-  session['p_hand'] = p_hand
-  redirect '/blackjack/gameover', locals: {d_hand: d_hand, p_hand: p_hand} if bust?(p_hand)
+  session['d_hand'], session['p_hand'] = d_hand, p_hand
+
+  game_over if bust?(p_hand)
   erb :blackjack, locals: {d_hand: d_hand, p_hand: p_hand}
 end
 
 post '/blackjack/stay' do
   d_hand, p_hand = session['d_hand'], session['p_hand']
   dealer_plays(d_hand + p_hand, d_hand)
-  win = false
-  win = true if bust?(d_hand)
-  win = true if value_hand(p_hand) > value_hand(d_hand)
-  erb :stay, locals: {d_hand: d_hand, p_hand: p_hand, win: win}
+  win if bust?(d_hand)
+  win if value_hand(p_hand) > value_hand(d_hand)
+  tie if value_hand(p_hand) == value_hand(d_hand)
+  game_over
 end
 
-get '/blackjack/gameover' do
-  erb :gameover, locals: {d_hand: session['d_hand'], p_hand: session['p_hand']}
+get '/blackjack/result/:message' do
+  erb :result, locals: {p_hand: session['p_hand'], d_hand: session['d_hand'], message: params[:message]}
 end
