de59b145b0f62ffe25ffdbf0ee96f95e1be3ca12
ayva <olga.ayva@gmail.com>
2015-07-15 15:40:11 -0700

    fixing payback the bet

--- app.rb
+++ app.rb
@@ -26,23 +26,23 @@ end
 
 get "/blackjack" do
 
-  
+  session[:bet] = params[:bet].to_i
+  session[:bankroll]-=params[:bet].to_i
   erb :game , :locals => {:dealercards => session[:dealercards], 
                          :playercards => session[:playercards], 
                          :gamestate => session[:gamestate],
-                         :bankroll => session[:bankroll]
+                         :bankroll => session[:bankroll],:bet => session[:bet]
                        }
 
 end
 
 post "/blackjack" do
-  session[:bet] = params[:bet].to_i
-  session[:bankroll]-=params[:bet].to_i
+  
   
   if session[:gamestate] == true
   session[:dealercards] = []
   session[:playercards] = []
-  session[:bet] = 0
+  #session[:bet] = 0
   # session[:bankroll] = 1000
   end
 
@@ -64,23 +64,25 @@ post "/blackjack" do
 
   #Check for winner
   if game.game_over(choice)
+       session[:message] = game.game_over(choice)
     case session[:message]
     when "Winner!"
       session[:bankroll] += 2*session[:bet]
     when "Tie!"
+
       session[:bankroll] += session[:bet]
     end
 
     session[:gamestate] = true
 
-    session[:message] = game.game_over(choice)
+    
 
     redirect '/blackjack/stay'
   else
     save_game(game.dealercards, game.playercards, session[:gamestate])  
   end
 
-  erb :game, :locals => {:dealercards => game.dealercards, :playercards => game.playercards, :gamestate => session[:gamestate], :bankroll => session[:bankroll]}
+  erb :game, :locals => {:dealercards => game.dealercards, :playercards => game.playercards, :gamestate => session[:gamestate], :bankroll => session[:bankroll], :bet => session[:bet]}
 
 #playercards, dealercards, winner = Game(playercards,dealercards)
 
