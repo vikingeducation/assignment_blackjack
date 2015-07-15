c2f358260c6f3a4b36c30b61c82d577f0e0325f4
David <davidmeza@outlook.com>
2015-07-15 13:46:32 -0400

    working on hitting and staying

--- blackjack.rb
+++ blackjack.rb
@@ -19,18 +19,31 @@ end
 
 #load initial hands and render them
 get '/blackjack' do
-	@deck = Deck::DealHands.new
-	session[:dealer_hand] = @deck.dealer_hand.to_json
-	session[:player_hand] = @deck.player_hand.to_json
-  	erb :blackjack
+  if session[:player_hand]
+    @deck = Deck::DealHands.new(JSON.parse(session[:player_hand]),
+                JSON.parse(session[:dealer_hand]),
+                JSON.parse(session[:deck]))
+  else
+  	@deck = Deck::DealHands.new
+  	session[:dealer_hand] = @deck.dealer_hand.to_json
+  	session[:player_hand] = @deck.player_hand.to_json
+    session[:deck] = @deck.game_deck.to_json
+  end
+  erb :blackjack
 end
 
 #user can select to hit or stay
 post '/blackjack/hit' do
 	@deck = Deck::DealHands.new(JSON.parse(session[:player_hand]),
-								JSON.parse(session[:dealer_hand]))
+								JSON.parse(session[:dealer_hand]),
+                JSON.parse(session[:deck]))
+  @deck.deal_to_player
 	#conditional logic to calculate card_value
-	# if @deck.count_hand_value() > 21
+  if @deck.count_hand_value(@deck.player_hand) > 21
+    redirect '/blackjack/stay'
+	else
+    redirect '/blackjack'
+  end
 end
 
 #user
--- helpers/deal_hands.rb
+++ helpers/deal_hands.rb
@@ -4,26 +4,29 @@ class DealHands
 
   CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 1]
 
-  attr_reader :player_hand, :dealer_hand, :deck
+  attr_reader :player_hand, :dealer_hand, :game_deck
 
-  def initialize(player_hand = [], dealer_hand = [])
+  def initialize(player_hand = [], dealer_hand = [], game_deck = [])
 
     @player_hand = player_hand
     @dealer_hand = dealer_hand
-    build_deck
-    first_deal if @player_hand.empty? && @dealer_hand.empty?
+    @game_deck = game_deck
+    if @player_hand.empty? && @dealer_hand.empty?
+      build_deck
+      first_deal
+    end
   end
 
   def build_deck
 
-    @deck = CARDS * 4
-    @deck.shuffle!
+    @game_deck = CARDS * 4
+    @game_deck.shuffle!
 
   end
 
   def deal_to_player
 
-    @player_hand << @deck.pop
+    @player_hand << @game_deck.pop
 
   end
 
@@ -38,7 +41,7 @@ class DealHands
 
   def deal_to_dealer
 
-    @dealer_hand << @deck.pop
+    @dealer_hand << @game_deck.pop
 
   end
 
