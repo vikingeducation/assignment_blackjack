fad8564c722cd3e8b36a4f61a1938a2f90794d97
shadefinale <shadefinale@gmail.com>
2015-07-15 11:46:59 -0500

    Checking for busting basic functionality works.

--- app.rb
+++ app.rb
@@ -1,6 +1,41 @@
 
 require 'sinatra'
 require 'erb'
+require './blackjack.rb'
+require 'pry'
+
+enable :sessions
+
+include Blackjack
+
+helpers do
+
+  def initial_hands
+    hand1 = []
+    hand2 = []
+    deal([], hand1)
+    deal(hand1, hand2)
+    deal(hand1 + hand2, hand1)
+    deal(hand1 + hand2, hand2)
+
+    return hand1, hand2
+  end
+
+  def card_to_hand(card)
+    faces = {1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King"}
+    suits = {0 => "spades", 1 => "clubs", 2 => "hearts", 3 => "diamonds"}
+    ret = ""
+    if faces[(card % 13) + 1]
+      value = faces[(card % 13) + 1]
+    else
+      value = (card % 13) + 1
+    end
+    ret += value.to_s
+    ret += " of "
+    ret += suits[(card / 13)]
+  end
+
+end
 
 
 get '/' do
@@ -8,5 +43,22 @@ get '/' do
 end
 
 get '/blackjack' do
-  erb :blackjack
+  d_hand, p_hand = initial_hands
+  session['d_hand'] = d_hand
+  session['p_hand'] = p_hand
+  erb :blackjack, locals: {d_hand: d_hand, p_hand: p_hand}
+end
+
+post '/blackjack/hit' do
+  d_hand, p_hand = session['d_hand'], session['p_hand']
+  deal(d_hand + p_hand, p_hand)
+  session['d_hand'] = d_hand
+  session['p_hand'] = p_hand
+  redirect '/blackjack/stay', locals: {d_hand: d_hand, p_hand: p_hand} if bust(p_hand)
+  erb :blackjack, locals: {d_hand: d_hand, p_hand: p_hand}
+end
+
+get '/blackjack/stay' do
+  d_hand, p_hand = session['d_hand'], session['p_hand']
+  erb :stay, locals: {d_hand: d_hand, p_hand: p_hand}
 end
--- blackjack.rb
+++ blackjack.rb
@@ -1,17 +1,17 @@
 module Blackjack
   DECK = (0..51).to_a
-  
+
   def deal(dealt_cards, target_hand)
     cards = DECK - dealt_cards
     cards = cards.shuffle
     target_hand.push(cards.sample)
     target_hand
-  end 
+  end
 
   def bust (hand)
     hand.reduce(0){|acc, val| acc += (val % 13 + 1)} > 21
   end
-  
+
   def dealer_hand
     until total_hand < 17
       hit(total_hand)
