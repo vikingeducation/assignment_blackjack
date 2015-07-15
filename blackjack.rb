4f5f23fabdf4ff7e90320ddaeb2ac8d432ad4dea
alokp <alokpra@gmail.com>
2015-07-15 11:53:44 -0700

    basic game working

--- blackjack.rb
+++ blackjack.rb
@@ -11,6 +11,18 @@ set :server, "thin"
 enable :sessions
 helpers Deck
 
+# helpers do
+
+# 	def new_game
+# 		@deck = Deck::DealHands.new
+# 	end
+
+# 	def load_game(player_hand, dealer_hand, deck)
+# 		@deck = Deck::DealHands.new(player_hand, dealer_hand, deck)
+# 	end
+
+# end
+
 
 #kick off with user pushing button to play
 get '/' do
@@ -19,15 +31,15 @@ end
 
 #load initial hands and render them
 get '/blackjack' do
-  if session[:player_hand]
-    @deck = Deck::DealHands.new(JSON.parse(session[:player_hand]),
-                JSON.parse(session[:dealer_hand]),
-                JSON.parse(session[:deck]))
+  if session[:game_deck] && session[:dealer_hand] && session[:player_hand]
+   	@deck = Deck::DealHands.new(JSON.parse(session[:player_hand]),
+    				JSON.parse(session[:dealer_hand]),
+                	JSON.parse(session[:game_deck]))
   else
-  	@deck = Deck::DealHands.new
+  	@deck = @deck = Deck::DealHands.new
   	session[:dealer_hand] = @deck.dealer_hand.to_json
   	session[:player_hand] = @deck.player_hand.to_json
-    session[:deck] = @deck.game_deck.to_json
+    session[:game_deck] = @deck.game_deck.to_json
   end
   erb :blackjack
 end
@@ -35,9 +47,9 @@ end
 #user can select to hit or stay
 post '/blackjack/hit' do
 	@deck = Deck::DealHands.new(JSON.parse(session[:player_hand]),
-								JSON.parse(session[:dealer_hand]),
-                JSON.parse(session[:deck]))
-  @deck.deal_to_player
+					JSON.parse(session[:dealer_hand]),
+                	JSON.parse(session[:game_deck]))
+  	session[:player_hand] = @deck.deal_to_player.to_json
 	#conditional logic to calculate card_value
   if @deck.count_hand_value(@deck.player_hand) > 21
     redirect '/blackjack/stay'
@@ -47,6 +59,14 @@ post '/blackjack/hit' do
 end
 
 #user
-post '/blackjack/stay' do
-
+get '/blackjack/stay' do
+	@deck = Deck::DealHands.new(JSON.parse(session[:player_hand]),
+					JSON.parse(session[:dealer_hand]),
+                	JSON.parse(session[:game_deck]))
+	if @deck.count_hand_value(@deck.player_hand) < 21
+		@deck.deal_to_dealer until @deck.count_hand_value(@deck.dealer_hand) >= 17
+	end
+	results = @deck.check_who_won
+	session.clear
+	erb :stay, :locals => {results: results}
 end
--- helpers/deal_hands.rb
+++ helpers/deal_hands.rb
@@ -7,7 +7,6 @@ class DealHands
   attr_reader :player_hand, :dealer_hand, :game_deck
 
   def initialize(player_hand = [], dealer_hand = [], game_deck = [])
-
     @player_hand = player_hand
     @dealer_hand = dealer_hand
     @game_deck = game_deck
@@ -18,37 +17,44 @@ class DealHands
   end
 
   def build_deck
-
     @game_deck = CARDS * 4
     @game_deck.shuffle!
-
   end
 
   def deal_to_player
-
     @player_hand << @game_deck.pop
-
   end
 
   def first_deal
-
     2.times do
       deal_to_dealer
       deal_to_player
     end
-
   end
 
   def deal_to_dealer
-
     @dealer_hand << @game_deck.pop
-
   end
 
   def count_hand_value(hand)
     hand.inject(0) {|sum, card_val| sum + card_val}
   end
 
+  def check_who_won
+    player_score = count_hand_value(@player_hand)
+    dealer_score = count_hand_value(@dealer_hand)
+    if player_score > 21
+      "You Busted"
+    elsif dealer_score > 21
+      "Dealer Busted. You Win!"
+    elsif player_score == dealer_score
+      "Draw"
+    elsif player_score > dealer_score
+      "You won"
+    else
+      "Dealer won"
+    end
+  end
 end
 
 end
