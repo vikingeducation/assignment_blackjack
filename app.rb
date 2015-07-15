d25e9630e012007b0177f07491a0e09c0acb942e
shadefinale <shadefinale@gmail.com>
2015-07-15 12:36:14 -0500

    Basic game working, does not check for initial blackjacks.

--- app.rb
+++ app.rb
@@ -27,12 +27,19 @@ post '/blackjack/hit' do
   deal(d_hand + p_hand, p_hand)
   session['d_hand'] = d_hand
   session['p_hand'] = p_hand
-  redirect '/blackjack/gameover', locals: {d_hand: d_hand, p_hand: p_hand} if bust(p_hand)
+  redirect '/blackjack/gameover', locals: {d_hand: d_hand, p_hand: p_hand} if bust?(p_hand)
   erb :blackjack, locals: {d_hand: d_hand, p_hand: p_hand}
 end
 
 post '/blackjack/stay' do
   d_hand, p_hand = session['d_hand'], session['p_hand']
-  erb :stay, locals: {d_hand: d_hand, p_hand: p_hand}
-  
+  dealer_plays(d_hand + p_hand, d_hand)
+  win = false
+  win = true if bust?(d_hand)
+  win = true if value_hand(p_hand) > value_hand(d_hand)
+  erb :stay, locals: {d_hand: d_hand, p_hand: p_hand, win: win}
+end
+
+get '/blackjack/gameover' do
+  erb :gameover, locals: {d_hand: session['d_hand'], p_hand: session['p_hand']}
 end
--- blackjack.rb
+++ blackjack.rb
@@ -2,61 +2,57 @@ module Blackjack
   DECK = (0..51).to_a
 
   def deal(dealt_cards, target_hand)
-    cards = DECK - dealt_cards
-    cards = cards.shuffle
+    cards = (DECK - dealt_cards).shuffle
     target_hand.push(cards.sample)
-    target_hand
   end
-  
+
   def initial_hands
-    hand1 = []
-    hand2 = []
+    hand1, hand2 = [], []
+    deal_initial_cards(hand1, hand2)
+    return hand1, hand2
+  end
+
+  def deal_initial_cards(hand1, hand2)
     deal([], hand1)
     deal(hand1, hand2)
     deal(hand1 + hand2, hand1)
     deal(hand1 + hand2, hand2)
-
-    return hand1, hand2
   end
-  
 
   #strictly for showing the hand to player
   def card_to_hand(card)
     faces = {1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King"}
     suits = {0 => "spades", 1 => "clubs", 2 => "hearts", 3 => "diamonds"}
     ret = ""
-    if faces[(card % 13) + 1]
-      value = faces[(card % 13) + 1]
-    else
-      value = (card % 13) + 1
-    end
-    ret += value.to_s
-    ret += " of "
-    ret += suits[(card / 13)]
+    faces[(card % 13) + 1].nil? ? value = (card % 13) + 1 : value = faces[(card % 13) + 1]
+    ret += value.to_s + " of " + suits[(card / 13)]
   end
 
-  def bust (hand)
-    hand.reduce(0){|acc, val| acc += (val % 13 + 1)} > 21
+  def bust? (hand)
+    value_hand(hand) > 21
   end
-  
+
+  def blackjack?(hand)
+    value_hand(hand) == 21
+  end
+
   def value_hand(hand)
     aces, other = hand.partition{|val| val % 13 == 0}
-    sum = other.reduce(0){|acc, val| acc += (val % 13 + 1)}
-    until aces.empty?
-      current = aces.pop
-      if sum + 11 > 21
-        sum += 1
-      else
-        sum += 11
-      end
+    digit, face = other.partition{|val| val % 13 < 9}
+
+    sum = digit.reduce(0){|acc, val| acc += (val % 13 + 1)}
+
+    sum += 10 * face.length
+
+    aces.length.times do
+      (sum + 11 > 21) ? sum += 1 : sum += 11
     end
     sum
   end
 
-  def dealer_hand
-    until total_hand < 17
-      hit(total_hand)
+  def dealer_plays(dealt_cards, hand)
+    until value_hand(hand) >= 17
+      deal(dealt_cards, hand)
     end
-
   end
 end
