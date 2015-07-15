b8fc8f7a8dc56d392b5ec704f2293252d7a4b1d4
shadefinale <shadefinale@gmail.com>
2015-07-15 16:46:19 -0500

    Added scorekeeping until session ends. Users get 3:2 winnings if they get a blackjack.

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ enable :sessions
 helpers do
   include Blackjack
 
+  # When the initial cards are dealt, check for blackjacks.
   def check_winner(d_hand,p_hand)
 
     if blackjack?(d_hand) && blackjack?(p_hand)
@@ -16,20 +17,34 @@ helpers do
     elsif blackjack?(d_hand) && !blackjack?(p_hand)
       game_over
     elsif blackjack?(p_hand)
-      win
+      blackjack
     end
 
   end
 
+  # game_over/win/lose/blackjack all set the 'ingame' flag to false to
+  # denote that the player is finished with their current game.
+
   def game_over
+    session['ingame'] = false
     redirect "/blackjack/result/You Lose..."
   end
 
   def win
+    session['ingame'] = false
+    session['money'] += session['bet'] * 2
     redirect "/blackjack/result/You Win!"
   end
 
+  def blackjack
+    session['ingame'] = false
+    session['money'] += session['bet'] * 2.5
+    redirect "/blackjack/result/Blackjack!"
+  end
+
   def tie
+    session['ingame'] = false
+    session['money'] += session['bet']
     redirect "/blackjack/result/You Pushed!"
   end
 end
@@ -39,15 +54,52 @@ get '/' do
   'hello world'
 end
 
+# Give the player money if they've just opened the page.
+get '/bet' do
+  session['money'] = 1000 unless session['money']
+  erb :bet, locals: {msg: nil, money: session['money']}
+end
+
+get '/bet/error' do
+  session['money'] = 1000 unless session['money']
+  erb :bet, locals: {msg: "Not enough money.", money: session['money']}
+end
+
+# Redirect to blackjack if a game is in progress.
+# If the player tries to bet more money than they have,
+# Redirect them to the betting page with an error.
+post '/bet' do
+  redirect '/blackjack' if session['ingame']
+  if session['money'] && session['money'] >= params[:amt].to_i
+    session['bet'] = params[:amt].to_i
+    session['money'] -= params[:amt].to_i
+    redirect '/blackjack'
+  else
+    redirect '/bet/error'
+  end
+end
+
+# If they're in a game, load the dealer and player hands.
+# Else, generate the hands, and say that they player
+# is now in a game.
 get '/blackjack' do
-  d_hand, p_hand = initial_hands
-  session['d_hand'] = d_hand
-  session['p_hand'] = p_hand
+  if session['ingame']
+    d_hand = session['d_hand']
+    p_hand = session['p_hand']
+  else
+    d_hand, p_hand = initial_hands
+    session['d_hand'] = d_hand
+    session['p_hand'] = p_hand
+    session['ingame'] = true
+  end
 
   check_winner(d_hand,p_hand)
   erb :blackjack, locals: {d_hand: d_hand, p_hand: p_hand}
 end
 
+# When the player hits, deal them a card.
+# Update the player and dealer hands.
+# Redirect the player to the results screen if they've busted.
 post '/blackjack/hit' do
   d_hand, p_hand = session['d_hand'], session['p_hand']
   deal(d_hand + p_hand, p_hand)
@@ -57,6 +109,10 @@ post '/blackjack/hit' do
   erb :blackjack, locals: {d_hand: d_hand, p_hand: p_hand}
 end
 
+# When the player decides to stay, have the dealer hit until 17.
+# Then, the player wins if the dealer busts or has a lower hand value.
+# The player and dealer tie if they have equal hand values.
+# Otherwise, the player loses.
 post '/blackjack/stay' do
   d_hand, p_hand = session['d_hand'], session['p_hand']
   dealer_plays(d_hand + p_hand, d_hand)
@@ -66,6 +122,7 @@ post '/blackjack/stay' do
   game_over
 end
 
+# Generic results screen that can display win/lose/push message.
 get '/blackjack/result/:message' do
-  erb :result, locals: {p_hand: session['p_hand'], d_hand: session['d_hand'], message: params[:message]}
+  erb :result, locals: {p_hand: session['p_hand'], d_hand: session['d_hand'], message: params[:message], money: session['money']}
 end
--- blackjack.rb
+++ blackjack.rb
@@ -1,17 +1,24 @@
 module Blackjack
   DECK = (0..51).to_a
+  FACES = {1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King"}
+  SUITS = {0 => "spades", 1 => "clubs", 2 => "hearts", 3 => "diamonds"}
 
+  # Deal the target hand a card from the remaining cards
+  # in the deck.
+  # Dealt cards should be any player and dealer hands.
   def deal(dealt_cards, target_hand)
     cards = (DECK - dealt_cards).shuffle
     target_hand.push(cards.sample)
   end
 
+  # Returns two hands with two cards each.
   def initial_hands
     hand1, hand2 = [], []
     deal_initial_cards(hand1, hand2)
     return hand1, hand2
   end
 
+  # Deal two cards each to two hands alternatingly.
   def deal_initial_cards(hand1, hand2)
     deal([], hand1)
     deal(hand1, hand2)
@@ -21,11 +28,9 @@ module Blackjack
 
   #strictly for showing the hand to player
   def card_to_hand(card)
-    faces = {1 => "Ace", 11 => "Jack", 12 => "Queen", 13 => "King"}
-    suits = {0 => "spades", 1 => "clubs", 2 => "hearts", 3 => "diamonds"}
     ret = ""
-    faces[(card % 13) + 1].nil? ? value = (card % 13) + 1 : value = faces[(card % 13) + 1]
-    ret += value.to_s + " of " + suits[(card / 13)]
+    FACES[(card % 13) + 1].nil? ? value = (card % 13) + 1 : value = FACES[(card % 13) + 1]
+    ret += value.to_s + " of " + SUITS[(card / 13)]
   end
 
   def bust? (hand)
@@ -36,20 +41,22 @@ module Blackjack
     value_hand(hand) == 21
   end
 
+  # Split a hand into ace, face, and other cards.
+  # Total up the other and face card values.
+  # Then, add the maximum score of aces unless it would bust
+  # the hand, then add minimum values for remaining aces.
   def value_hand(hand)
     aces, other = hand.partition{|val| val % 13 == 0}
     digit, face = other.partition{|val| val % 13 < 9}
 
     sum = digit.reduce(0){|acc, val| acc += (val % 13 + 1)}
-
     sum += 10 * face.length
+    aces.length.times {(sum + 11 > 21) ? sum += 1 : sum += 11}
 
-    aces.length.times do
-      (sum + 11 > 21) ? sum += 1 : sum += 11
-    end
     sum
   end
 
+  # The dealer hits until they have 17 or more card value.
   def dealer_plays(dealt_cards, hand)
     until value_hand(hand) >= 17
       deal(dealt_cards, hand)
