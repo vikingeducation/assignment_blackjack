85614863bb9dc3546fb2983d48a85577e3a8b3d9
Justin Mullis <justin.mullis@gmail.com>
2015-09-15 20:58:31 -0400

    working on persisting deck

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 #!/usr/bin/env ruby
 require 'sinatra'
+require 'json'
+enable :sessions
+
+require './deck.rb'
+
+# require './helpers/deck_helper.rb'
+
+# helpers DeckHelper
 
 get '/' do
   erb :home
+end
+
+get '/blackjack' do
+  saved_deck = session[:deck].nil? ? nil : JSON.parse( session[:deck] )
+  @deck = Deck.new(saved_deck).cards
+  session[:deck] = @deck.to_json
+  erb :blackjack
 end
