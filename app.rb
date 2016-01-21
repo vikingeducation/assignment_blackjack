66ca13ea69f1d314144f82ef99f1bba8343069c9
Deepa Kackar <dkackar99@yahoo.com>
2016-01-21 10:17:18 -0800

    Added bkackjack.erb vars

--- app.rb
+++ app.rb
@@ -9,13 +9,20 @@ require './helpers/blackjack'
 enable :sessions
 
 get '/' do
-  erb :index
-end
-
-get '/blackjack' do
   game = Blackjack.new
   session['player_deck'] = game.player_cards.to_json
   session['dealer_deck'] = game.dealer_cards.to_json
   session['main_deck'] = game.deck.to_json
 
+  erb :index
+
+end
+
+post '/' do
+  redirect to('/blackjack')
+end
+
+get '/blackjack' do
+  player_deck = JSON.parse( session[:player_deck] )
+  erb :blackjack, :locals => {:player_deck => player_deck}
 end
