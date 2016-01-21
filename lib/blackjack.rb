9b8727ca73a0b7f87a539e9022853ee60d7aad5b
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-21 13:14:27 -0500

    work on blackjack.rb

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,4 +1,12 @@
 class Blackjack
+
+  attr_accessor :player_hand, :dealer_hand
+
+  def initialize(player_hand,dealer_hand)
+    @player_hand = player_hand || Hand.new
+    @dealer_hand = dealer_hand || Hand.new
+  end
+
   SUITS = {
     'Spades' => "S",
     'Diamonds' => "D",
@@ -22,9 +30,54 @@ class Blackjack
     'King' => "K"
   }
 
+  def random_card
+    Card.new(rank: RANKS.keys.sample, suit: SUITS.keys.sample)
+  end
+
+  def first_deal
+    2.times do
+      deal(@player_hand)
+      deal(@dealer_hand)
+    end
+  end
+
+  def deal(player)
+    player.cards << random_card
+  end
+
 end
 
-class Deck
+class Hand
+  attr_accessor :cards
+
+  def self.deserialize(str)
+    hand = new
+    str.chars.each_slice(2) do |card|
+      suit = Blackjack::SUITS.invert[card[1]]
+      rank = Blackjack::RANKS.invert[card[0]]
+      hand.cards << Card.new(rank,suit)
+    end
+    hand
+  end
+
+  def initialize(cards)
+    @cards = []
+  end
+
+  def aces
+    @cards.select(&:ace?)
+  end
+
+  def best_value
+    total = @cards.inject(0) { |sum, card| sum += card.value }
+
+    aces.each do |ace|
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
 
 end
 
@@ -37,7 +90,10 @@ class Card
   def to_s
     "#{Blackjack::RANKS[@rank]}#{Blackjack::SUITS[@suit]}"
   end
+
+  def ace?
+    @rank == "Ace"
+  end
 end
 
 p Card.new(rank: "Ace", suit: "Spades").to_s
-
