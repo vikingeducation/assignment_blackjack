787861652e32deb6daab5824f7af49a9d13c6163
Steven Chang <prime_pork@hotmail.com>
2016-02-14 13:51:26 +1000

    moved the deck class to a separate file

--- app.rb
+++ app.rb
@@ -1,58 +1,19 @@
 =begin
-  2. Shuffle up and Deal
+  3. Refactor for OOP
 
-  The hand typically begins by placing a bet but we'll skip that functionality for now. There should be no styling done on the view templates.
+  Now that you've gotten the application working, refactor it into good object-oriented code. That means using objects and classes wherever possible. Remember that imported classes will not have access to the session or cookies!
 
-  1. Start the game by building a basic home template (blackjack.erb) which will show player and dealer hands (as well as total player bankroll and bet amounts later). Remember that these things will need to be persisted to the browser between requests. (DONE)
-
-  2. The get '/blackjack' route should shuffle up and deal hands to the dealer and the player, rendering the template with these things filled in. (DONE)
-
-  3. Create any supporting classes you need to make the game go.
-  Hint: a great method to explore for building the deck is product (DONE)
-
-  # I'm thinking he wants us to combine two arrays, one that goes from A to K and another that has the suits. (DONE)
-
-  4. The player's turn comes first. You should have buttons available to HIT or STAY.
-    HIT will go to post '/blackjack/hit', which adds a card to the player's hand and re-renders the main page. (DONE)
-
-  5. If the player hitting would bust that player (bring the total over 21 points), redirect to get /blackjack/stay. (DONE)
-
-  6. STAY will go to get '/blackjack/stay', which triggers the dealer to play his hand (hitting until 17). Once the dealer's hand is finished, render the main page with cards revealed and a message describing the result. (DONE)
-
-  7. Now stop and take a minute to examine your Request and Response objects in your terminal so you understand what is going on. Use a binding.pry or see This Stack Overflow Post for another way to check out the request. This is useful so you get an idea of where the cookies come from.
-  Don't forget to commit!
-
+  Your best option is to (re)instantiate the class by passing the necessary session/cookie information up front (e.g. Player.new(:bankroll => session["bankroll"])) and storing it in the instance variables for use during the current HTTP request.
 =end
 
 require 'sinatra'
 require 'json'
 require './helpers/blackjack_helpers'
+require './deck'
 
 helpers BlackjackHelpers
 
 enable :sessions
-
-class Deck
-  attr_reader :deck
-  # initialize a new deck
-  def initialize(deck=nil)
-    if deck == nil
-      @deck = ('2'..'10').to_a
-      ['J','Q','K','A'].each { |word_cards| @deck << word_cards }
-      @deck = @deck.product(['C','S','H','D']).shuffle
-    else
-      @deck = deck.shuffle
-    end
-  end
-end
-
-class Computer
-
-end
-
-class Player
-
-end
 
 get '/' do
   erb :home
---
+++ deck.rb
@@ -0,0 +1,11 @@
+class Deck
+  attr_reader :deck
+  # initialize a new deck
+
+  def initialize
+    @deck = ('2'..'10').to_a
+    ['J','Q','K','A'].each { |word_cards| @deck << word_cards }
+    @deck = @deck.product(['C','S','H','D']).shuffle
+  end
+
+end
