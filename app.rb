a8c1538565fe879c4a7ea30501be5bc5a6e49cb2
Stephen Mayeux <stephenmayeux@gmail.com>
2016-05-29 14:57:07 +0900

    fix spelling mistake in config

--- app.rb
+++ app.rb
@@ -2,3 +2,8 @@ require 'sinatra'
 require 'erb'
 require 'json'
 require 'pry-byebug'
+
+
+get '/' do
+  "<h1>Hello world!</h1>"
+end
--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
 require './app'
-run Sinatra::Aplication
+run Sinatra::Application
