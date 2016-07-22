001e674b225d88d037279bc2f957a81cc4302f07
Alex Lach <alexglach@gmail.com>
2016-07-22 12:40:58 -0400

    hands are dealt, working on buttons

--- app.rb
+++ app.rb
@@ -1,11 +1,35 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 require "pry"
+require_relative "deck"
+
+enable :sessions
 
 get "/" do
   erb :home
 end
 
 get "/blackjack" do
-  erb :blackjack
+
+  if session["deck"]
+    @deck = session["deck"]
+    @player_hand = session["player_hand"]
+    @dealer_hand = session["dealer_hand"]
+  else
+    @deck = Deck.new.deck
+    @player_hand = []
+    @dealer_hand = []
+    2.times do
+      @player_hand << @deck.pop
+      @dealer_hand << @deck.pop
+    end
+  end
+
+  session["deck"] = @deck
+  session["player_hand"] = @player_hand
+  session["dealer_hand"] = @dealer_hand
+
+
+  erb :blackjack 
 end
+
--- deck.rb
+++ deck.rb
@@ -1,17 +1,17 @@
 class Deck
 
+  attr_reader :deck
+
   def initialize(prev_deck = nil)
-    @deck = prevdeck || generate_deck
+    @deck = prev_deck || generate_deck
   end
 
   def generate_deck
     arr = %w(2 3 4 5 6 7 8 9 10 j q k a)
     deck = []
     4.times { deck << arr }
-    deck.flatten
+    deck.flatten.shuffle
   end
 
-  def deal
-  end
 
 end
