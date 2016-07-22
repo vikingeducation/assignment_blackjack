b98a91a0971677adfce3ac09901ec38fb359d4a3
Adrian Mui <adrian.mui@gmail.com>
2016-07-22 12:36:58 -0400

    setup

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+enable :sessions
+
 get '/' do
   erb :home
 end
 
 
 get '/blackjack' do
+ @deck = Deck.new
 
 end
---
+++ helpers/blackjack_helper.rb
@@ -0,0 +1,14 @@
+# ./helpers/checkers_helper.rb
+
+module BlackjackHelper
+
+  def save_deck(board)
+  end
+
+  def load_deck
+    Deck.new( JSON.parse( session[:deck_arr] ) )
+  end
+
+
+end
+
