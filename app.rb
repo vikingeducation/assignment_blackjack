66c569d6cc6409191c98a21f832d93c9501f2df7
essian <jessicampdennis@hotmail.com>
2016-07-22 19:43:48 +0100

    corrects for aces

--- app.rb
+++ app.rb
@@ -17,17 +17,18 @@ get '/blackjack' do
   player_cards = session['player_cards'] = deal(deck, 2)
   dealer_cards = session['dealer_cards'] = deal(deck, 2)
   erb :blackjack, locals: { player_cards: player_cards,
-                            dealer_cards: dealer_cards.first }
+                            dealer_cards: dealer_cards.first, wording: nil }
 end
 
 post '/blackjack/hit' do
   session['player_cards'] << deal(session['deck'], 1)
   player_cards = session['player_cards']
+  # player_cards = [[:ace,:spade],[:ace, :club],[10, :spade]]
   if bust?(player_cards)
     redirect('/blackjack/stay')
   else
     erb :blackjack, locals: {player_cards: session['player_cards'],
-                             dealer_cards: session['dealer_cards'].first }
+                             dealer_cards: session['dealer_cards'].first, wording: nil }
   end
 end
 
@@ -35,8 +36,16 @@ end
 
 # welcome -> form :player_name, creates a game instance, assigns the player name
 get '/blackjack/stay' do
-  #do stuff
 
-  erb :blackjack, locals: {player_cards: player_cards, dealer_cards: dealer_cards, message: message}
+  dealer_cards = session['dealer_cards']
+  while calc_value(dealer_cards) < 17 
+    dealer_cards << deal[session['deck'], 1]
+  end
+  compare_cards(dealer_cards, session['player_cards'])
+
+  #do stuff
+  wording = "You lose"
+  erb :blackjack, locals: {player_cards: session['player_cards'],
+                             dealer_cards: session['dealer_cards'], wording: wording }
 
 end
--- helpers/blackjack_helper.rb
+++ helpers/blackjack_helper.rb
@@ -4,7 +4,7 @@ module BlackjackHelper
   end
 
 
-  def calc_value(cards)
+  def cards_total(cards)
     cards.map do |card|
       if card[0].is_a?(Integer)
         card[0]
@@ -14,19 +14,27 @@ module BlackjackHelper
         10
       end
     end.reduce(:+)
+
+  end
+
+  def correct_for_aces(cards, total)
+    num_of_aces(cards).times do
+      total -=10 if total > 21
+    end
+    total
+  end
+
+  def calc_value(cards)
+    total = cards_total(cards)
+    correct_for_aces(cards, total)
   end
 
   def bust?(cards)
     total_value = calc_value(cards)
-    if total_value > 21 && no_aces?(cards)
+    if total_value > 21 
       true
-    elsif total_value < 21
+    elsif total_value <= 21
       false
-    else
-      num_of_aces.times do
-        total_value -= 10
-      end
-      total_value > 21
     end
   end
 
@@ -35,7 +43,7 @@ module BlackjackHelper
   end
 
   def num_of_aces(cards)
-    cards.select { |cards| card[0] == :ace }.length
+    cards.select { |card| card[0] == :ace }.length
   end
 
   def new_deck
