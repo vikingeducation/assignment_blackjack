4478ab06afcb6d9cc5763cfd25220d317690fc71
“Matthew <matthew.hinea@gmail.com>
2016-07-22 12:14:07 -0400

    set up basic sinatra functions

---
+++ app.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+require 'sinatra/contrib'
+
+get '/' do 
+  erb :home
+end
+
+get '/blackjack' do 
+
+  erb :blackjack
+end
+
+# Deck class
+  # #shuffle
+  # deck is a hash of cards and number of each card remaining
