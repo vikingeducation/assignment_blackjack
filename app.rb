56451132f90962c28c6d3e3ac5561b22d86da68a
Morgan Martin <morganm5201@gmail.com>
2016-07-22 13:55:25 -0400

    stuff

--- app.rb
+++ app.rb
@@ -16,26 +16,26 @@ get '/blackjack' do
   deck = session['deck'] = new_deck.shuffle
   player_cards = session['player_cards'] = deal(deck, 2)
   dealer_cards = session['dealer_cards'] = deal(deck, 2)
-  erb :blackjack, locals: { deck: deck, player_cards: player_cards,
+  erb :blackjack, locals: { player_cards: player_cards,
                             dealer_cards: dealer_cards.first }
 end
 
-post '/blackjack/hit' do 
-player_cards = session['player_cards']
-dealer_cards = session['dealer_cards']
-deck = session['deck']
-player_cards << deck.pop
-
-erb :blackjack, locals: {player_cards: player_cards, 
-                          dealer_cards: dealer_cards.first}
-
+post '/blackjack/hit' do
+  session['player_cards'] << deal(session['deck'], 1)
+  player_cards = session['player_cards']
+  if bust?(player_cards)
+    redirect('/blackjack/stay')
+  else
+    erb :blackjack, locals: {player_cards: session['player_cards'],
+                             dealer_cards: session['dealer_cards'].first }
   end
+end
 
 
 
 # welcome -> form :player_name, creates a game instance, assigns the player name
-get 'blackjack/stay' do
-#do stuff
+get '/blackjack/stay' do
+  #do stuff
 
   erb :blackjack, locals: {player_cards: player_cards, dealer_cards: dealer_cards, message: message}
 
--- helpers/blackjack_helper.rb
+++ helpers/blackjack_helper.rb
@@ -3,6 +3,41 @@ module BlackjackHelper
     deck.pop(num)
   end
 
+
+  def calc_value(cards)
+    cards.map do |card|
+      if card[0].is_a?(Integer)
+        card[0]
+      elsif card[0] == :ace
+        11
+      else
+        10
+      end
+    end.reduce(:+)
+  end
+
+  def bust?(cards)
+    total_value = calc_value(cards)
+    if total_value > 21 && no_aces?(cards)
+      true
+    elsif total_value < 21
+      false
+    else
+      num_of_aces.times do
+        total_value -= 10
+      end
+      total_value > 21
+    end
+  end
+
+  def no_aces?(cards)
+    !cards.map { |card| card[0] }.any? { |card| card == :ace }
+  end
+
+  def num_of_aces(cards)
+    cards.select { |cards| card[0] == :ace }.length
+  end
+
   def new_deck
     return_array = []
     ranks.product(suits)
