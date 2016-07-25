c8e4d01d20e8422aa6cbd4da35c340f4f20168d8
David Watts <davidjeremywatts@gmail.com>
2016-07-25 16:38:25 -0700

    functioning and oop blackjack

--- app.rb
+++ app.rb
@@ -12,23 +12,46 @@ get '/' do
 end
 
 get '/blackjack' do
-  new_deck
-  @phand = new_hand("player")
-  @dhand = new_hand("dealer")
-  @deck = session["deck"]
+  g = Game.new
+  @phand = g.phand.show
+  @dhand = g.dhand.show
+  @deck = g.deck
+  save_game(g)
+
+  # new_deck
+  # @phand = new_hand("player")
+  # @dhand = new_hand("dealer")
+  # @deck = session["deck"]
   erb :blackjack
 end
 
 post '/blackjack/hit' do
-  hit
-  sum = hand_sum("player")
-  @phand = session["playerhand"]
-  @dhand = session["dealerhand"]
-  @deck = session["deck"]
-  sum > 21 ? redirect '/blackjack/stay' : erb :blackjack
+
+  game = restore_game
+  game.hit(game.phand)
+  save_game(game)
+  @phand = game.phand.show
+  @dhand = game.dhand.show
+  @deck = game.deck
+  if game.phand.sum >= 21
+    redirect to('/blackjack/stay')
+  else
+    erb :blackjack
+  end
 
 end
 
 get '/blackjack/stay' do
+  @done = true
+  game = restore_game
+  until game.dhand.sum > 16
+    game.hit(game.dhand)
+  end
+  save_game(game)
+  @phand = game.phand.show
+  @dhand = game.dhand.show
+  @playersum = game.phand.sum
+  @dealersum = game.dhand.sum
+  erb :blackjack
 
 end
