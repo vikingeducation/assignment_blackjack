c1113061010c142e77b0c263086accd8368b9026
Conor Fischer <conor.fischer@gmail.com>
2016-09-28 17:50:59 -0500

    Remove dealer & Player

--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
-require "sinatra"
-require "sinatra/reloader" if development?
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'json'
+
+require './helpers/blackjack_helper.rb'
+require './public/assets/carddeck.rb'
+require './public/assets/player.rb'
+
+helpers BlackjackHelper
 
 enable :sessions
 
 get '/blackjack' do
+  @deck = load_deck
+  #@player = load_player
+  #@dealer = load_player
+
+  save_deck(@deck)
+  #save_player(@player)
+  #save_dealer(@dealer)
+
   erb :"blackjack.html"
 end
