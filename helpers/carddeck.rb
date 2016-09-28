7d71c179baa637d0fd257a889378627eb661e176
Conor Fischer <conor.fischer@gmail.com>
2016-09-27 22:48:15 -0500

    Add session loaders

--- helpers/carddeck.rb
+++ helpers/carddeck.rb
@@ -1,5 +1,17 @@
 module CardDeck
 
+  def save_deck
+    session["current_deck"] = create_deck
+  end
+
+  def load_deck
+    session["current_deck"]
+  end
+
+  def place_card
+    session["current_deck"].pop
+  end
+
   def create_deck
     card_faces = [*1..13]
     card_suits = %w(Spade Heart Club Diamond)
@@ -8,6 +20,7 @@ module CardDeck
         [face,suit]
       end
     end
-    deck.flatten!(1)
+    deck.flatten!(1).shuffle!
   end
+
 end
