3aab9df2ab2fb3764b1139153cdfce29023bb71c
Joe Bernardi <joebernardi@hotmail.com>
2016-09-27 22:24:31 -0700

    psuedo

--- app.rb
+++ app.rb
@@ -16,3 +16,48 @@ post '/blackjack' do
 	erb :blackjack
 
 end
+
+
+# cookies / sessions
+# the player cards and dealer cards will need to be persisted with each load of the blackjack page
+# each 'hit' or 'stay' sends new request
+# the players and dealers cards are saved
+# the cards in deck must be saved as well
+# LATER ITEMS
+	# the bet amount
+	# player bankroll
+
+# CLASSES
+	# Deck
+	# Computer AI
+	# Player - bankroll
+
+# blackjack page handles the functionality for the game
+# when the page is loaded the cards should be dealt to the player and dealer
+# a deck class could be implemented to handle the shuffling of the cards
+# the cards are then distributed to the player and dealer when the page loads
+	# the deck has 52 cards (A-K)
+	# the cards could be an array that is shuffled
+	# using product method, those cards could be distributed in that manner
+# the dealer's and player's cards are evaluated for blackjack - bets paid 3:2 if player win
+	# else the player forfeits bet
+# when the player hits
+	# another card is dealth to the player and evaluated
+		# Until the player busts or stays
+			# the players turn ends
+		# else
+			# the player can hit or stay
+		# end
+# the dealer's turn begins when the player finishes
+	# if the dealer cards < 17
+		# dealer hits
+	# else
+		# dealer stays if < 21
+	# end
+# dealer turn ends and player dealer cards are evaluated
+	# if the dealer < player
+		# bet pays 1:1
+	# else
+		# player loses bet
+	# end
+
