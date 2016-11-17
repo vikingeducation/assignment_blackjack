8a70a3bbf7517c93fbcecc54e7779d657e927f5f
Jake Tingley <jaket32@gmail.com>
2016-11-17 17:25:21 -0500

    pomodoro

--- app.rb
+++ app.rb
@@ -6,6 +6,34 @@ require 'pp'
 
 enable :sessions
 
+helpers do
+
+  def winner(dealer_hand, player_hand)
+    dealer_score = prox_twenty_one(dealer_hand)
+    player_score = prox_twenty_one(player_hand)
+
+    return "TIE!!!" if player_score < 1 && dealer_score < 1
+    return "TIE!!!" if player_score == dealer_score
+
+    return "Dealer busts Player wins" if dealer_score < 0
+    return "You bust, I win" if player_score < 0
+
+    if player_score < dealer_score
+      "You Win!!! Congratulations!"
+    else
+      "I Win!! Your money is mine!!"
+    end
+  end
+
+  def prox_twenty_one(hand_score)
+    if hand_score > 21
+      -1
+    else
+      21 - hand_score
+    end
+  end
+end
+
 get '/' do
   deck = Deck.new
   deck.shuffle!
@@ -37,19 +65,29 @@ get '/hit' do
   session["player_hand"] = player_hand
   session["deck"] = deck
 
-  redirect to('/blackjack')
+  if player_hand.value >= 21
+    redirect to('/stay')
+  else
+    redirect to('/blackjack')
+  end
 end
 
 get '/stay' do
   dealer_hand = session["dealer_hand"]
+  player_hand = session["player_hand"]
   deck = session["deck"]
 
   until dealer_hand.value >= 17
     dealer_hand.cards << deck.deal_card
   end
 
+  winning_message = winner(dealer_hand.value, player_hand.value)
+
   session["dealer_hand"] = dealer_hand
   session["deck"] = deck
 
-  redirect to('/blackjack')
+  erb :stay, :locals => {
+    :player_hand => session["player_hand"],
+    :dealer_hand => session["dealer_hand"],
+    :winning_message => winning_message }
 end
