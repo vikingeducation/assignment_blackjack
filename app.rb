86d20bf6ce8356c489e0d9383aeb289d81a0dc51
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-17 17:18:14 -0500

    Implement player hitting

--- app.rb
+++ app.rb
@@ -15,21 +15,36 @@ end
 
 get '/blackjack' do
 
+  # helper method?
   deck = Blackjack::Deck.new(cards: session['deck'])
-
   user_hand   = session['user_hand'] || deck.draw(2)
   dealer_hand = session['dealer_hand'] || deck.draw(2)
 
   user = Blackjack::User.new(hand: user_hand)
   dealer = Blackjack::Dealer.new(hand: dealer_hand)
 
-  # helper method me
+  # helper method?
   session['deck'] = deck.cards
   session['user_hand'] = user_hand
   session['dealer_hand'] = dealer_hand
 
   erb :blackjack, locals: { user: user,
-                            dealer: dealer }
+                            dealer: dealer,
+                            params: params }
+end
+
+post '/blackjack' do
+
+  if params[:hit]
+    deck = Blackjack::Deck.new(cards: session['deck'])
+    user = Blackjack::User.new(hand: session['user_hand'])
+    user.hand.concat(deck.draw(1))
+    session['user_hand'] = user.hand
+  elsif params[:stay]
+    session['stay'] = true
+  end
+
+  redirect to '/blackjack'
 end
 
 # get '/blackjack/hit'
