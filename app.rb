5c57f65b3786dd111af099929f2fce69d00c0643
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-17 18:55:58 -0600

    completed blackjack

--- app.rb
+++ app.rb
@@ -21,7 +21,8 @@ get '/blackjack' do
   dealer_hand = session['dealer_hand'] || deck.draw(2)
 
   # updat user initialization to include betting pool
-  user = Blackjack::User.new(hand: user_hand)
+  user = Blackjack::User.new(hand: user_hand,
+                             money: session['user_cash'])
   dealer = Blackjack::Dealer.new(hand: dealer_hand,
                                  deck: deck,
                                  opponent: user)
@@ -30,9 +31,13 @@ get '/blackjack' do
   session['deck']        = deck.cards
   session['dealer_hand'] = dealer_hand
   session['user_bet']    = session['user_bet'] || 500
-  session['user_cash']   = (user.betting_pool - session['user_bet'].to_i)
-  user.betting_pool      = session['user_cash']
   session['user_hand']   = user_hand
+
+  # updates betting pool
+  unless session['disabled']
+    session['user_cash']   = (user.betting_pool - session['user_bet'].to_i)
+    user.betting_pool      = session['user_cash']
+  end
 
   # if user stays
   if session['stay']
@@ -42,12 +47,18 @@ get '/blackjack' do
 
   # check for bust
   if user.bust? || dealer.bust?
+    if dealer.bust?
+      user.betting_pool += (session['user_bet'].to_i * 2)
+      session['user_cash'] = user.betting_pool
+    end
     session['bet']         = nil
     session['dealer_hand'] = nil
     session['deck']        = nil
     session['disabled']    = nil
     session['user_hand']   = nil
   end
+
+
 
   erb :blackjack, locals: { bet: session['user_bet'].to_i,
                             user: user,
@@ -62,11 +73,11 @@ post '/blackjack' do
     user = Blackjack::User.new(hand: session['user_hand'])
     user.hand.concat(deck.draw(1))
     session['user_hand'] = user.hand
-    session['disabled']  = ' disabled'
+    session['disabled']  = 'disabled'
     session['user_bet']  = params[:bet]
   elsif params[:stay]
     session['stay']      = true
-    session['disabled']  = ' disabled'
+    session['disabled']  = 'disabled'
   end
 
   redirect to '/blackjack'
