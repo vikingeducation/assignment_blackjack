b548a7c92bee66b3b90d4bdad4703eb6df3be572
Ann Allan <ann.cristina.allan@gmail.com>
2017-04-28 11:33:18 -0400

    Add human player totals

--- app.rb
+++ app.rb
@@ -33,83 +33,95 @@ def update_bank(bank, op, amount)
   return updated_bank
 end
 
-def render(dealer_reveal, num_players, ai)
-  puts @shoe.length
-  if dealer_reveal
-    puts "Dealer total:  #{@dealer.total}."
-    puts "Dealer cards:  #{dealer_hand}"
-  else
-    puts "Dealer total showing:  #{dealer_total_showing}."
-    puts "Dealer cards:  #{dealer_showing}"
-  end
-  num_players.times do |x|
-    puts "#{@players[x].name}'s bet: #{@players[x].bet}, chips: #{@players[x].chips},  total: #{@players[x].total}."
-    if @players[x].insurance_bet > 0
-      puts "#{@players[x].name}'s' insurance bet: #{@players[x].insurance_bet}."
-    end
-    puts "#{@players[x].name}'s' cards:  #{player_hand(@players[x].hand)}"
-    if !@players[x].split_hand.empty?
-      puts "#{@players[x].name}'s' split cards:  #{player_hand(@players[x].split_hand)}"
-    end
-  end
-  if ai
-    puts "#{@comp.name}'s bet:  #{@comp.bet}, chips:  #{@comp.chips}, total:  #{@comp.total}."
-    puts "#{@comp.name}'s cards:  #{player_hand(@comp.hand)}"
-    if !@comp.split_hand.empty?
-      puts "#{@comp.name}'s' split cards:  #{player_hand(@comp.split_hand)}"
-    end
-  end
-end #render method
 
-def dealer_hand #Helper for render
-  dealer_hand_string = ""
-  @dealer.hand.each do |x|
-    dealer_hand_string += "#{x.name}  "
-  end
-  return dealer_hand_string
+def ai_bet
 end
 
-def dealer_total_showing #Helper for render
+def player_total(hand)
   tot = 0
-  if !@dealer.hand.nil?
-    @dealer.hand.each do |dealer_card|
-      if !dealer_card.nil?
-        tot += dealer_card.value
-      else
-        tot += 0
-      end
-    end
-    face_down = @dealer.hand[1]
-    if !face_down.nil?
-      tot -= face_down.value
-    else
-      tot -= 0
-    end
-  else
-    tot = 0
+  hand.each do |card|
+    tot += card.value
   end
   return tot
-end #dealer_total_showing method
+end
 
-def dealer_showing #Helper for render
-  if !@dealer.hand[0].nil?
-    "#{@dealer.hand[0].name}  ??"
-  else
-     "-- --"
-  end
-end # dealer_showing method
-
-def player_hand(hand)
-  if !hand[0].nil?
-    player_hand_string = ""
-    hand.each do |x|
-      player_hand_string += "#{x.name}  "
-    end
-    return player_hand_string
-  else
-    "-- --"
-  end
-end  # player_hand method
+# def render(dealer_reveal, num_players, ai)
+#   puts @shoe.length
+#   if dealer_reveal
+#     puts "Dealer total:  #{@dealer.total}."
+#     puts "Dealer cards:  #{dealer_hand}"
+#   else
+#     puts "Dealer total showing:  #{dealer_total_showing}."
+#     puts "Dealer cards:  #{dealer_showing}"
+#   end
+#   num_players.times do |x|
+#     puts "#{@players[x].name}'s bet: #{@players[x].bet}, chips: #{@players[x].chips},  total: #{@players[x].total}."
+#     if @players[x].insurance_bet > 0
+#       puts "#{@players[x].name}'s' insurance bet: #{@players[x].insurance_bet}."
+#     end
+#     puts "#{@players[x].name}'s' cards:  #{player_hand(@players[x].hand)}"
+#     if !@players[x].split_hand.empty?
+#       puts "#{@players[x].name}'s' split cards:  #{player_hand(@players[x].split_hand)}"
+#     end
+#   end
+#   if ai
+#     puts "#{@comp.name}'s bet:  #{@comp.bet}, chips:  #{@comp.chips}, total:  #{@comp.total}."
+#     puts "#{@comp.name}'s cards:  #{player_hand(@comp.hand)}"
+#     if !@comp.split_hand.empty?
+#       puts "#{@comp.name}'s' split cards:  #{player_hand(@comp.split_hand)}"
+#     end
+#   end
+# end #render method
+#
+# def dealer_hand #Helper for render
+#   dealer_hand_string = ""
+#   @dealer.hand.each do |x|
+#     dealer_hand_string += "#{x.name}  "
+#   end
+#   return dealer_hand_string
+# end
+#
+# def dealer_total_showing #Helper for render
+#   tot = 0
+#   if !@dealer.hand.nil?
+#     @dealer.hand.each do |dealer_card|
+#       if !dealer_card.nil?
+#         tot += dealer_card.value
+#       else
+#         tot += 0
+#       end
+#     end
+#     face_down = @dealer.hand[1]
+#     if !face_down.nil?
+#       tot -= face_down.value
+#     else
+#       tot -= 0
+#     end
+#   else
+#     tot = 0
+#   end
+#   return tot
+# end #dealer_total_showing method
+#
+# def dealer_showing #Helper for render
+#   if !@dealer.hand[0].nil?
+#     "#{@dealer.hand[0].name}  ??"
+#   else
+#      "-- --"
+#   end
+# end # dealer_showing method
+#
+# def player_hand(hand)
+  # if !hand[0].nil?
+  #   player_hand_string = ""
+  #   hand.each do |x|
+  #     player_hand_string += "#{x.name}  "
+  #   end
+  #   return player_hand_string
+  # else
+  #   "-- --"
+  # end
+# end  # player_hand method
 
 class Card
   attr_accessor :value, :rank, :suit, :name
@@ -134,6 +146,7 @@ post '/blackjack' do
   session["num_players"].times do |x|
     session["player#{x}_hand"] = deal(shoe)
     session["player#{x}_bank"] = 1000
+    session["player#{x}_total"] = player_total(session["player#{x}_hand"])
   end
   session["ai"] = params[:ai]
   if session["ai"]
@@ -144,6 +157,7 @@ post '/blackjack' do
 end
 
 post '/bet' do
+  session["ai_bet"] = ai_bet
   session["player#{session["turn"]}_bet"] = params[:bet]
   session["player#{session["turn"]}_bank"] =  update_bank(session["player#{session["turn"]}_bank"].to_i, "subtract", session["player#{session["turn"]}_bet"].to_i)
 
