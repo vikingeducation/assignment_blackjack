2313c69c627414455913c075a6b9f7882bbbcaf6
Chad Lucas <chad.lucas.19@gmail.com>
2017-05-11 04:22:19 -0500

    add check_if_face_card? method

--- blackjack.rb
+++ blackjack.rb
@@ -1,5 +1,6 @@
+require 'pry'
 class Blackjack
-	attr_accessor :deck, :player_hand, :computer_hand, :card
+	attr_accessor :deck, :player_hand, :computer_hand, :card, :score, :face_card
 	SUIT = %w(clubs diamonds spades hearts)
 	VALUE = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 	Card = Struct.new(:suit, :value)
@@ -32,7 +33,8 @@ class Blackjack
   	  @deck.shuffle!
   		@player_hand << @deck.pop
     end
-      @player_hand
+     	@player_hand
+     	puts @player_hand
   end
 
   def deal_computer_hand
@@ -42,11 +44,38 @@ class Blackjack
   	  @computer_hand
   end
 
+  #def get_player_score
+  	#check_if_face_card?(@player_hand)
+  	#check_if_ace?
+    #@score = @player_hand[0].value.to_i + @player_hand[1].value.to_i
+    #puts @score
+  #end
+
+  def get_player_score
+  	puts @player_hand
+  	@player_hand.map do |card|
+  		card.value = check_if_ace?(card)
+  		card.value = check_if_face_card?(card)
+  	end
+  end
+
+  def check_if_face_card?(card)
+  	if card.value == "J" || card.value == "Q" || card.value == "K"
+  		 card.value = 10
+  	end
+  	card
+  end
+
+
+ 
+
+
 end
 
 
 game = Blackjack.new
 
 game.deal_cards
+game.get_player_score
 
 
