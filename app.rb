b4fed7595422ba21b3c0d4c6c15880ca073231fb
Roy Chen <roychenlei@gmail.com>
2017-05-17 15:36:29 +0800

    enabled sessions and added code to load/save objects' state

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra/reloader' if development?
 require 'erb'
 require 'pry-byebug'
 
+enable :sessions
+
 class Deck
   attr_reader :cards
 
@@ -54,13 +56,18 @@ end
 
 # main game route
 get '/blackjack' do
-  @deck = Deck.new
-  @player = Player.new
-  @dealer = Dealer.new
+  @deck = Deck.new(session[:deck_cards])
+  @player = Player.new(session[:player_hand])
+  @dealer = Dealer.new(session[:dealer_hand])
 
   # deal cards to Dealer and Player
-  2.times { @player.hand << @deck.deal_card }
-  2.times { @dealer.hand << @deck.deal_card }
+  2.times { @player.hand << @deck.deal_card } if @player.hand.empty?
+  2.times { @dealer.hand << @deck.deal_card } if @dealer.hand.empty?
+
+  # save objects' state to session
+  session[:deck_cards] = @deck.cards
+  session[:player_hand] = @player.hand
+  session[:dealer_hand] = @dealer.hand
 
   # main game view
   erb :blackjack
