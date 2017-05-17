76655006f78d5f2f525a5228fd5bc827c02bb4ac
Roy Chen <roychenlei@gmail.com>
2017-05-17 14:49:00 +0800

    added Deck class

--- app.rb
+++ app.rb
@@ -3,6 +3,25 @@ require 'sinatra/reloader' if development?
 require 'erb'
 require 'pry-byebug'
 
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = generate_cards
+  end
+
+  # generates a standard 52 card deck
+  def generate_cards
+    suits = [:spades, :hearts, :clubs, :diamonds]
+    values = (2..10).to_a
+    values << [:ace, :jack, :queen, :king]
+    values.flatten!
+
+    values.product(suits).shuffle
+  end
+end
+
+
 # root route
 get '/' do
   erb :home
@@ -10,6 +29,8 @@ end
 
 # main game route
 get '/blackjack' do
+  @deck = Deck.new
+
   # main game view
   erb :blackjack
 end
