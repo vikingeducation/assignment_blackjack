d9a1d784fb06a4116a1d0b10ca50cb1007af2b7e
Roy Chen <roychenlei@gmail.com>
2017-05-19 19:04:30 +0800

    added more helper methods

--- helpers/blackjack_helpers.rb
+++ helpers/blackjack_helpers.rb
@@ -1,26 +1,32 @@
 module BlackjackHelpers
-  def save_dealer
+  def save_dealer(dealer_hand)
+    session[:dealer_hand] = dealer_hand
   end
 
   def load_dealer
+    Dealer.new(session[:dealer_hand])
   end
 
-  def save_player
+  def save_player(player_hand)
+    session[:player_hand] = player_hand
   end
 
   def load_player
+    Player.new(session[:player_hand])
   end
 
-  def save_deck
+  def save_cards(cards)
+    session[:cards] = cards
   end
 
-  def load_deck
+  def load_cards
+    Blackjack.new(session[:cards])
   end
 
   # helper method to reset game
   def reset_game
     session[:dealer_hand] = nil
-    session[:deck_cards] = nil
     session[:player_hand] = nil
+    session[:cards] = nil
   end
 end
