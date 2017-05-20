18ed6dd14b90af0c09f75ebec5b21c5ab579674a
Roy Chen <roychenlei@gmail.com>
2017-05-20 10:58:52 +0800

    added instance variable and options hash

--- helpers/player.rb
+++ helpers/player.rb
@@ -2,12 +2,21 @@
 
 module BlackjackHelpers
   class Player
-    attr_accessor :hand,
-                  :balance
+    attr_accessor :hand
 
-    def initialize(hand = nil, balance = nil)
-      @hand = hand || []
-      @balance = balance || 1000
+    attr_reader :balance,
+                :current_bet
+
+    def initialize(options = {})
+      @hand = options[:hand] || []
+      @balance = options[:balance] || 1000
+      @current_bet = options[:current_bet] || 0
+    end
+
+    # places a bet
+    def place_bet(bet)
+      @current_bet = bet
+      @balance -= bet
     end
   end
 end
