f72154fb08a9ce2be328048358e708b65a913f6e
andie5 <andieasa@googlemail.com>
2017-06-04 23:42:33 +0100

    basic functionality of user cards and dealers' cards displayed

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require 'erb'
 require 'pry-byebug'
+require './helpers/card_helper.rb'
+
+# Register our SecretHelper module so it's available
+# here and in our views
+helpers CardHelper
 
 enable :sessions
 
@@ -13,5 +18,39 @@ end
 
 get '/blackjack' do
   # Game starts here
+  generate_cards
+  
+  2.times do
+    start_game
+  end
+
+  @cards = load_cards
+  @player_score = load_score
+
+  @cards2 = load_cards2
+  @dealer_score = load_score2
   erb :blackjack
 end
+
+
+
+
+# post '/blackjack/hit' do
+#   deal
+#   @cards = load_cards
+#   @player_score = load_score
+
+#   # If the player hitting would bust that player (bring the total over 21 points), redirect to get /blackjack/stay
+#   redirect to("blackjack/stay")
+# end
+
+
+# triggers the dealer to play his hand (hitting until 17). Once the dealer's hand is finished, render the main page with cards revealed and a message describing the result.
+# get '/blackjack/stay'
+#   deal2
+#   @cards2 = load_cards2
+#   @dealer_score = load_score2
+
+#    # Render the template with that secret
+#   erb :"blackjack/stay"
+# end 
--- helpers/card_helper.rb
+++ helpers/card_helper.rb
@@ -1,83 +1,137 @@
 # ./helpers/card_helper.rb
 module CardHelper
 
-  # Save the secret to the session hash
-  # Again, the `self` is implicit for `self.text`
-  def save_cards(secret_text)
-    session[:secret_text] = secret_text
-  end
+  # # Save the secret to the session hash
+  # # Again, the `self` is implicit for `self.text`
+  # def save_cards(secret_text)
+  #   session[:secret_text] = secret_text
+  # end
 
-  def initialize
     @player_score = 0 
     @combinations = []
-    @current_player = @player
+    # @current_player = @player
     @game_over = false
-    @player = Player.new
-    @dealer = Dealer.new
-  end
+    # @player = Player.new
+    # @dealer = Dealer.new
 
 
   def generate_cards
+
+    @player_score = 0 
+    @dealer_score = 0
+    @combinations = []
+    # @current_player = @player
+    @game_over = false
+    @player_cards = []
+    @dealer_cards = []
+
     number =  [2,3,4,5,6,7,8,9,10,10,10,10]
     # need to deal with aces separatley
     suit = %w{diamonds clubs hearts spades}
-    @combinations = number.product(suit)
+    # Simulate the real shuffling of cards
+    @combinations = number.product(suit).shuffle
   
   #    %w{2 3 4 5 6 7 8 9 10 J Q K A}.product(%w{diamonds clubs hearts spades}).shuffle
   end
+
  
   def deal
-     2.times do
-       s = @combinations.sample
-       @combinations.delete(s)
-       calculate_score(s[0])
-     end
+     s = @combinations.sample
+     save_cards(s)
+     puts "#{s} cards selected"
+     @combinations.delete(s)
+     calculate_score(s[0])
   end
 
-  def game_over?
-    @player.score >= 21 || @dealer.score >= 21 || @combinations.length <= 1
+  def save_cards(cards)
+    @player_cards << cards
+    session[:cards] = @player_cards
   end
+
+
+  
+  # def game_over?
+  #   @player.score >= 21 || @dealer.score >= 21 || @combinations.length <= 1
+  # end
  
   def calculate_score(n)
     @player_score += n
+    session[:player_score] = @player_score
   end
 
-  def process_input(player_choice)
-    if(player_choice == "STAY")
-      @current_player = @dealer
-    elsif(player_choice == "HIT")
-      @current_player.deal
-    end
+
+  def deal2
+     s = @combinations.sample
+     save_cards2(s)
+     puts "#{s} cards selected"
+     @combinations.delete(s)
+     calculate_score2(s[0])
   end
+
+  def save_cards2(cards)
+    @dealer_cards << cards
+    session[:cards2] = @dealer_cards
+  end
+
+
+  def calculate_score2(n)
+    @dealer_score += n
+    session[:dealer_score] = @dealer_score
+  end
+
+
+  # def process_input(player_choice)
+  #   if(player_choice == "STAY")
+  #     @current_player = @dealer
+  #   elsif(player_choice == "HIT")
+  #     @current_player.deal
+  #   end
+  # end
 
   def start_game
-    @player.deal
-    @dealer.deal
+    # @player.deal
+    # @dealer.deal
+
+    deal
+    deal2
   end
 
   def get_player_choice
     # use session varible - use radio button to get input on the webpage
   end
 
-  def play
-    start_game
-    while !game_over?
-      user_choice = @current_player.get_player_choice
-      process_input(user_choice)
-      
-    end
-    quit_msg
-  end
+  # def play
+  #   start_game
+  #   while !game_over?
+  #     user_choice = @current_player.get_player_choice
+  #     process_input(user_choice)
+  #   end
+  #   quit_msg
+  # end
 
 
-  IN DEALER method - the user input should always be HIT of has 17 or more
+  # IN DEALER method - the user input should always be HIT of has 17 or more
 
-  def quit_msg
-    if(player.score >= 21 || @player.score <)
-  end
+  # def quit_msg
+  #   if(@current_player.score >= 21 || @current_player.score <)
+  # end
 
-  # Load the secret from the session
   def load_cards
-    session[:secret_text]
+    session[:cards]
   end
+
+  def load_score
+    session[:player_score]
+  end
+
+  def load_cards2
+    session[:cards2]
+  end
+
+  def load_score2
+    session[:dealer_score]
+  end
+
+
+
 end
