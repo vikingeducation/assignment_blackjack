e745324402b164609aa86822722af6b893f37d34
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-07-20 09:57:42 +0200

    working on getting deck to properly delete cards, stay button

--- app.rb
+++ app.rb
@@ -1,25 +1,115 @@
 require 'sinatra'
 require 'erb'
+require 'json'
 require 'pry-byebug'
 require 'sinatra/reloader' if development?
+
+enable :sessions
+
+helpers do
+
+  def build_deck
+    face = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+    suit = ["hearts", "spades", "clubs", "diamonds"]
+    @deck = face.product(suit).shuffle!
+  end
+
+  def deal_cards(num_of_cards)
+    hand = @deck.sample(num_of_cards)
+    @deck.delete_if.any? { |card| card == hand }
+    hand
+  end
+
+  def save_variables
+    session[:dealer] = @dealer
+    session[:player] = @player
+    session[:deck] = @deck
+  end
+
+  def restore_variables
+    @dealer = session[:dealer]
+    @player = session[:player]
+    @deck = session[:deck]
+  end
+
+  def get_player_score(player)
+    scores = {
+      '2' => 2,
+      '3' => 3,
+      '4' => 4,
+      '5' => 5,
+      '6' => 6,
+      '7' => 7,
+      '8' => 8,
+      '9' => 9,
+      '10' => 10,
+      'J' => 10,
+      'Q' => 10,
+      'K' => 10,
+      'A' => 11
+    }
+    score = player.reduce(0) do |s, i|
+      s += scores[i[0].to_s]
+    end
+
+    if score > 21 && player.any? {|x| x[0] == 'A'}
+      score = score - 10
+    end
+
+    score
+  end
+
+end
+
+before do
+  build_deck
+end
 
 
 get '/' do
   erb :home
 end
 
+
+# needs to shuffle deck and deal hands to dealer and player
 get '/blackjack' do
-  # needs to shuffle deck and deal hands to dealer and player
-  erb :blackjack # renders these things
+  @dealer = deal_cards(2)
+  @player = deal_cards(2)
+  @player_score = get_player_score(@player)
+  @dealer_score = get_player_score(@dealer)
+  if @player_score == 21 || @dealer_score == 21
+    erb :blackjack_win
+  else
+    save_variables
+    erb :blackjack # renders these things
+  end
 end
 
-post '/blackjack/hit' do
+post "/blackjack/hit" do
   # adds a card to the players hand and re-renders the main page
   # if hitting would bust player (over 21 total) redirect to get /blackjack/stay
-  # need to use cookies to keep track of cards already dealt?
+  # need to use cookies/session to keep track of cards already dealt?
+  restore_variables
+  @player << deal_cards(1).flatten
+  puts "#{@player.class}"
+  @player_score = get_player_score(@player)
+  @dealer_score = get_player_score(@dealer)
+  save_variables
+  if @player_score >= 21
+    redirect to('/blackjack/stay')
+  end
+  erb :blackjack
 end
 
-post '/blackjack/stay' do
+get '/blackjack/stay' do
   # gets dealer to play their hand - either hit or stay depending on if product is 17 or higher
   # render main page with all cards revealed and describes the result
+  restore_variables
+  @player_score = get_player_score(@player)
+  loop do
+    @dealer << deal_cards(1).flatten
+    @dealer_score = get_player_score(@dealer)
+    break if @dealer_score >= 17
+  end
+  erb :blackjack
 end
