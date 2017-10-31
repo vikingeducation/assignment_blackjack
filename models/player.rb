a73c4c1d3c318ee7a174c0af965b8c835dab2d9a
anne <richardson.ae@gmail.com>
2017-10-30 21:08:21 -0500

    Move calculate_hand to private

--- models/player.rb
+++ models/player.rb
@@ -11,19 +11,6 @@ class Player
     @bankroll = 100
   end
 
-  def calculate_hand
-    @hand.reduce(0) do |memo, card|
-      value = if card[0].class == Integer
-        card[0]
-      elsif Deck::FACE_CARDS.include?(card[0])
-        10
-      else
-        1
-      end
-      memo + value
-    end #hand
-  end
-
   def set_hand_value
     @hand_value = calculate_hand
   end
@@ -41,6 +28,19 @@ class Player
     @bankroll += bet
   end
 
+  private
+  def calculate_hand
+    @hand.reduce(0) do |memo, card|
+      value = if card[0].class == Integer
+        card[0]
+      elsif Deck::FACE_CARDS.include?(card[0])
+        10
+      else
+        1
+      end
+      memo + value
+    end #hand
+  end
 end
 
 class Dealer < Player
